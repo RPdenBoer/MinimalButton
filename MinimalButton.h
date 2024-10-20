5ed4abb426341e6b17efb949b4dd98254fe9e239
RPdenBoer <94905251+RPdenBoer@users.noreply.github.com>
2024-10-20 15:52:28 +0800

    fixes

--- MinimalButton.h
+++ MinimalButton.h
@@ -9,19 +9,21 @@ enum PressLength
     SHORT_PRESS,
     LONG_PRESS,
     SUPER_PRESS,
-    DOUBLE_PRESS,
-    CONSTANT_PRESS
+    DOUBLE_PRESS
 };
 
 template <uint32_t pin>
 class MinimalButton
 {
-    static MinimalButton<pin> *instance;
+    static volatile bool currentState;
+    static volatile uint32_t changeTime;
+
+    typedef void (*CallbackAll)(PressLength);
+    typedef void (*CallbackSingle)(void);
 
 public:
     MinimalButton(bool useInterrupt = true) : _useInterrupt(useInterrupt)
     {
-        instance = this;
     }
 
     void begin()
@@ -29,11 +31,10 @@ public:
         init();
     }
 
-    bool begin(uint16_t newTinyDelay, uint16_t newShortDelay, uint16_t newDoubleDelay, uint16_t newLongDelay, uint16_t newSuperDelay)
+    bool begin(uint16_t newShortDelay, uint16_t newDoubleDelay, uint16_t newLongDelay, uint16_t newSuperDelay)
     {
-        if (newTinyDelay < newShortDelay && newLongDelay > newShortDelay && newSuperDelay > newLongDelay && newShortDelay + newDoubleDelay < newLongDelay)
+        if (newLongDelay > newShortDelay && newSuperDelay > newLongDelay && newShortDelay + newDoubleDelay < newLongDelay)
         {
-            _TinyDelay = newTinyDelay;
             _ShortDelay = newShortDelay;
             _DoubleDelay = newDoubleDelay;
             _LongDelay = newLongDelay;
@@ -45,49 +46,74 @@ public:
         }
         else
         {
-            Serial.println("error: invalid delay values");
             return false;
         }
     }
 
-    void clear()
-    {
-        _currentPress = NO_PRESS;
-    }
-
     PressLength process()
     {
+        PressLength returnVal = NO_PRESS;
+
         if (!_useInterrupt)
         {
-            updatePress();
+            currentState = !digitalRead(pin);
+            changeTime = millis();
         }
 
-        uint32_t currentTime = millis();
-        if (_currentState && (currentTime - _pressTime > _SuperDelay) && !_constantReported)
+        if (currentState && !_lastState)
         {
-            _currentPress = CONSTANT_PRESS;
-            _constantReported = true;
+            _pressTime = changeTime;
+            _lastState = true;
+        }
+        else if (!currentState && _lastState)
+        {
+            _releaseTime = changeTime;
+            _lastState = false;
+
+            uint32_t deltaTime = _releaseTime - _pressTime;
+
+            if (deltaTime >= _ShortDelay && deltaTime < _LongDelay)
+            {
+                if (_pressPending && _releaseTime - _lastShortTime <= _DoubleDelay)
+                {
+                    returnVal = DOUBLE_PRESS;
+                    _pressPending = false;
+                }
+                else
+                {
+                    _lastShortTime = _releaseTime;
+                    _pressPending = true;
+                }
+            }
+            else if (deltaTime >= _LongDelay && deltaTime < _SuperDelay)
+            {
+                returnVal = LONG_PRESS;
+            }
+            else if (deltaTime >= _SuperDelay)
+            {
+                returnVal = SUPER_PRESS;
+            }
         }
 
-        PressLength returnVal = _currentPress;
-
-        clear();
+        if (_pressPending && millis() - _lastShortTime > _DoubleDelay)
+        {
+            returnVal = SHORT_PRESS;
+            _pressPending = false;
+        }
 
         return returnVal;
     }
 
-private:
-#ifdef ESP32
-    static void IRAM_ATTR interruptHandler()
-#else
-#ifdef ARDUINO_ARCH_STM32
-    __attribute__((section(".data"))) static void interruptHandler()
-#else
-    static void interruptHandler()
-#endif
-#endif
+    bool state()
     {
-        instance->updatePress();
+        return currentState;
+    }
+
+private:
+    static void interruptHandler()
+    {
+        currentState = !digitalRead(pin);
+        changeTime = millis();
     }
 
     void init()
@@ -97,88 +123,30 @@ private:
         {
             attachInterrupt(digitalPinToInterrupt(pin), interruptHandler, CHANGE);
         }
-    }
-
-    void updatePress()
-    {
-        uint32_t currentTime = millis();
-
-        if (_currentPress == NO_PRESS)
+        else
         {
-            _currentState = !digitalRead(pin);
-            _changeTime = currentTime;
-
-            if (_currentState && !_lastState)
-            {
-                _pressTime = _changeTime;
-                _lastState = true;
-                _constantReported = false;
-            }
-            else if (!_currentState && _lastState)
-            {
-                _releaseTime = _changeTime;
-                _lastState = false;
-
-                unsigned long deltaTime = _releaseTime - _pressTime;
-
-                if (deltaTime < _ShortDelay)
-                {
-                    if (deltaTime >= _TinyDelay)
-                    {
-                        _currentPress = TINY_PRESS;
-                    }
-                }
-                else if (deltaTime >= _ShortDelay && deltaTime < _LongDelay)
-                {
-                    if (_pressPending && (_releaseTime - _lastShortTime <= _DoubleDelay))
-                    {
-                        _currentPress = DOUBLE_PRESS;
-                        _pressPending = false;
-                    }
-                    else
-                    {
-                        _lastShortTime = _releaseTime;
-                        _pressPending = true;
-                    }
-                }
-                else if (deltaTime >= _LongDelay && deltaTime < _SuperDelay)
-                {
-                    _currentPress = LONG_PRESS;
-                }
-                else if (deltaTime >= _SuperDelay)
-                {
-                    _currentPress = SUPER_PRESS;
-                }
-            }
-
-            // Check for pending short press
-            if (_pressPending && (currentTime - _lastShortTime > _DoubleDelay))
-            {
-                _currentPress = SHORT_PRESS;
-                _pressPending = false;
-            }
         }
     }
 
-    volatile PressLength _currentPress = NO_PRESS;
-    volatile bool _currentState = false;
-    volatile bool _lastState = false;
-    volatile uint32_t _changeTime = 0;
-    volatile bool _constantReported = false;
-
     bool _useInterrupt = true;
+
+    // bool currentState = false;
+    bool _lastState = false;
+
     bool _pressPending = false;
 
     uint32_t _pressTime;
     uint32_t _releaseTime;
     uint32_t _lastShortTime;
 
-    uint16_t _TinyDelay = 1;
-    uint16_t _ShortDelay = 50;
+    uint16_t _ShortDelay = 30;
     uint16_t _DoubleDelay = 250;
     uint16_t _LongDelay = 500;
     uint16_t _SuperDelay = 1500;
 };
 
 template <uint32_t pin>
-MinimalButton<pin> *MinimalButton<pin>::instance = nullptr;
+volatile bool MinimalButton<pin>::currentState;
+
+template <uint32_t pin>
+volatile uint32_t MinimalButton<pin>::changeTime;
